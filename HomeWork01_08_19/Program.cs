6a844dbb70252608b6b53314de9cfbe7ad7e97cd
alisherKak <alisher.alish333@gmail.com>
2019-07-25 12:23:08 +0600

    HomeWork01.08.19

--- HomeWork01_08_19/Program.cs
+++ HomeWork01_08_19/Program.cs
@@ -1,15 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HomeWork01_08_19
 {
+    //Facade
+    public interface ISubsystem
+    {
+        string Operation();
+    }
+
+    public class DataBaseSubSystem : ISubsystem
+    {
+        public string Operation()
+        {
+            return "This subsystem wroks with database";
+        }
+    }
+
+    public class FileSubSystem : ISubsystem
+    {
+        public string Operation()
+        {
+            return "This subsystem wroks with file";
+        }
+    }
+
+    public class Facade
+    {
+        private DataBaseSubSystem _dataBaseSubSystem;
+        private FileSubSystem _fileSubSystem;
+
+        public Facade(DataBaseSubSystem dataBaseSubSystem, FileSubSystem fileSubSystem)
+        {
+            _dataBaseSubSystem = dataBaseSubSystem;
+            _fileSubSystem = fileSubSystem;
+        }
+
+        public string Operation()
+        {
+            string result = "Facade initializes subsystems:\n";
+            result += _dataBaseSubSystem.Operation() + "\n";
+            result += _fileSubSystem.Operation() + "\n";
+            return result;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            DataBaseSubSystem dataBaseSubSystem = new DataBaseSubSystem();
+            FileSubSystem fileSubSystem = new FileSubSystem();
+            Facade facade = new Facade(dataBaseSubSystem, fileSubSystem);
+
+            Console.WriteLine(facade.Operation());
         }
     }
 }
---
+++ Proxy/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Proxy
+{
+    //Proxy
+    public interface IEnter
+    {
+        void Enter();
+    }
+
+    public class Enterer : IEnter
+    {
+        public void Enter()
+        {
+            Console.WriteLine("Logined to the system");
+        }
+    }
+
+    public class EnterProxy : IEnter
+    {
+        private Enterer _enterer;
+
+        public void Enter()
+        {
+            if(CheckPassword())
+            {
+                _enterer = new Enterer();
+                _enterer.Enter();
+            }
+        }
+
+        public bool CheckPassword()
+        {
+            Console.WriteLine("This method checks password");
+
+            return true;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EnterProxy enterProxy = new EnterProxy();
+            enterProxy.Enter();
+        }
+    }
+}
